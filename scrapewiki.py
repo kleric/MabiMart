2ba199bb4a83e9872ca7809a4cef259062998097
Jemmin Chang <jemminchang@cmu.edu>
2014-06-14 13:38:35 -0400

    Second draft, still missing Enchant Types etc.
    Fixed several bugs in selectors, so now it works smoothly for the Amulet
    and Knight Lance pages. Still to add: Enchant Types, Other Information,
    Obtained From, and Sold By.

--- scrapewiki.py
+++ scrapewiki.py
@@ -5,44 +5,45 @@ from cssselect import HTMLTranslator, SelectorError
 
 def mw_item_page_scrape(url):
   mw_base_url = 'http://wiki.mabinogiworld.com'
-  r = requests.get(url) # fetch desired webpage to scrape
-  css_path_to_table = 'div#mw-content-text > table.mabitable:nth-of-type(1)'
-  # the CSS path to the Base Stats and Information table body; apparently, this thing
-  # ignores the <tbody> tag, and considers the <td> as direct children of <table>
+  r = requests.get(url) # request desired webpage to scrape
   if r.status_code != requests.codes.ok: # confirm that request was successful
     sys.exit('Request error')
   
   tree = html.fromstring(r.text) # parse html into tree
-  
+
+  # CSS selectors for desired items
+  table_css = 'div#mw-content-text > h2:contains("Base Stats and Information") + table.mabitable'
+  # the CSS path to the Base Stats and Information table body; apparently, this thing
+  # ignores the <tbody> tag, and considers the <td> as direct children of <table>
+  description_css = '#mw-content-text > p:nth-of-type(1)'
+  description_bullets_css = description_css + ' + ul > li'
+  icon_css = table_css + ' > tr:nth-child(1) > td.image > a > img'
+  all_th_css = table_css + ' > tr:not(tr:nth-child(1)) > th'
+  all_td_css = all_th_css + ' + td'
+  enchant_types_css = table_css + ' tr > th:contains("Enchant Types")'
+  other_info_css = table_css + ' tr > th:contains("Other Information")'
+
+  translator = HTMLTranslator()
   try: # generate XPaths using CSS selector strings
-    description_path = HTMLTranslator().css_to_xpath('#mw-content-text > p')
-    description_bullets_path = HTMLTranslator().css_to_xpath('#mw-content-text > p + ul > li')
-    icon_image_path = HTMLTranslator().css_to_xpath(css_path_to_table + ' > tr:nth-child(1) > td.image > a > img') + '/@src'
-    all_th_path = HTMLTranslator().css_to_xpath(css_path_to_table + ' > tr:not(tr:nth-child(1)) > th')
-    all_td_path = HTMLTranslator().css_to_xpath(css_path_to_table + ' > tr:not(tr:nth-child(1)) > th + td')
-    enchant_types_path = HTMLTranslator().css_to_xpath(css_path_to_table + ' tr > th:contains("Enchant Types")')
-    other_info_path = HTMLTranslator().css_to_xpath(css_path_to_table + ' tr > th:contains("Other Information")')
+    description_xpath = translator.css_to_xpath(description_css)
+    description_bullets_xpath = translator.css_to_xpath(description_bullets_css)
+    icon_link_xpath = translator.css_to_xpath(icon_css) + '/@src'
+    all_th_xpath = translator.css_to_xpath(all_th_css)
+    all_td_xpath = translator.css_to_xpath(all_td_css)
+    enchant_types_xpath = translator.css_to_xpath(enchant_types_css)
+    other_info_xpath = translator.css_to_xpath(other_info_css)
   
   except SelectorError:
     print('Invalid selector.')
   
   # find elements matching XPaths; all are lists
-  description = tree.xpath(description_path)
-  description_bullets = tree.xpath(description_bullets_path)
-  icon_image = tree.xpath(icon_image_path)
-  all_th = tree.xpath(all_th_path)
-  all_td = tree.xpath(all_td_path)
-  enchant_types = tree.xpath(enchant_types_path)
-  other_info = tree.xpath(other_info_path)
-  
-  for d in description:
-    print 'Description:', d.text_content()
-  
-  for db in description_bullets:
-    print ' *', db.text_content()
-  
-  for ii in icon_image:
-    print 'Icon link:', mw_base_url + ii
+  description = tree.xpath(description_xpath)
+  description_bullets = tree.xpath(description_bullets_xpath)
+  icon_link = tree.xpath(icon_link_xpath)
+  all_th = tree.xpath(all_th_xpath)
+  all_td = tree.xpath(all_td_xpath)
+  enchant_types = tree.xpath(enchant_types_xpath)
+  other_info = tree.xpath(other_info_xpath)
   
   # check that there is only one th "Enchant Types" and one th "Other Information",
   # and that these are also selected in all_th
@@ -75,10 +76,20 @@ def mw_item_page_scrape(url):
     # set dictionary entry
     stats[all_th[i].text_content().strip()] = all_td[i].text_content().strip()
   
+  # Here's all the data, finally compiled in a dictionary
+  data = {'Description': description[0].text_content().strip(), 'Description Bullets': [b.text_content().strip() for b in description_bullets], 'Icon Link': mw_base_url + icon_link[0], 'Stats': stats}
+
+  return data
+
+def print_scrape(scrape_data):
+  print 'Description:', scrape_data['Description']
+  for db in scrape_data['Description Bullets']:
+    print ' o ' + db
+  print 'Icon link:', scrape_data['Icon Link']
+  print 'Stats:'
+  stats = scrape_data['Stats']
   for key in stats:
     print key + ':', stats[key]
-  
-  print "All Done"
 
-mw_item_page_scrape('http://wiki.mabinogiworld.com/view/Amulet')
-mw_item_page_scrape('http://wiki.mabinogiworld.com/view/Knight_Lance')
+print_scrape(mw_item_page_scrape('http://wiki.mabinogiworld.com/view/Amulet'))
+print_scrape(mw_item_page_scrape('http://wiki.mabinogiworld.com/view/Knight_Lance'))
