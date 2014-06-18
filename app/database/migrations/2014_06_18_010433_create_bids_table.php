51a672dc91e09c230c2ee05aef497f1d41035b88
Clark Chen <clark@kleric.org>
2014-06-17 22:12:01 -0400

    Added bid fields

--- app/database/migrations/2014_06_18_010433_create_bids_table.php
+++ app/database/migrations/2014_06_18_010433_create_bids_table.php
@@ -13,10 +13,19 @@ class CreateBidsTable extends Migration {
 	public function up()
 	{
 		Schema::dropIfExists('bids');
-		
+
 		Schema::create('bids', function(Blueprint $table)
 		{
 			$table->increments('id');
+
+			$table->integer('auction_id')->unsigned();
+
+			$table->foreign('auction_id')->references('id')->on('auctions');
+
+			$table->integer('amount')->unsigned();
+
+			$table->integer('bidder_id')->unsigned();
+			$table->foreign('bidder_id')->references('id')->on('users');
 
 			$table->timestamps();
 		});
