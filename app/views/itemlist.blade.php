991b378f76e0a6eb555ef431401378bbe0cb924e
Clark Chen <clark@kleric.org>
2014-06-20 15:45:20 -0400

    Lists all item list

--- app/views/itemlist.blade.php
+++ app/views/itemlist.blade.php
@@ -10,25 +10,17 @@ Items
   		<li>All Items</li>
 	</ol>
 	<div class="list-group">
-  		<a href="#" class="list-group-item clearfix">
+		@foreach ($itemlist as $item) {
+  		<a href="/item/view/{{{ $item->id }}}" class="list-group-item clearfix">
   			<div class="item-thumbnail pull-left">
-    			<center><img src="images/items/thumbnails/2.png"></center>
+    			<center><img src="/images/items/{{{ $item->id }}}.png"></center>
     		</div>
     		<div>
-	    		<b class="list-group-item-heading">Fine Reforging Tool</b>
-	    		<i class="list-grou-item-text"> A highly sophisticated reforging tool meant for further reforging of an item. Reforging stats will not be changed.</i>
+	    		<b class="list-group-item-heading">{{{ $item->name }}}</b>
+	    		<i class="list-group-item-text">{{{ $item->description }}}</i>
 	    	</div>
-Can only be used on items that have been reforged at least once.</i>
   		</a>
-  		<a href="#" class="list-group-item clearfix">
-  			<div class="item-thumbnail pull-left">
-    			<center><img src="images/items/thumbnails/3.png"></center>
-    		</div>
-    		<div>
-    			<b class="list-group-item-heading">Nuadha Robe (F)</b>
-    			<i class="list-grou-item-text">A robe that has been imbued with Nuadha's power. Nuadha's power is said to have been split amongst his robe, gauntlets, and boots. You can become more powerful as a Demigod if you equip all three items.</i>
-  			</div>
-  		</a>
+  		@endforeach
 	</div>
 	
 @stop
