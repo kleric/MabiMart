89d89a908f8df3c4b305a5c7eba3b557381f6831
Clark Chen <clark@kleric.org>
2014-06-20 16:51:01 -0400

    Gave more space for notes

--- app/database/migrations/2014_06_12_000708_create_items_table.php
+++ app/database/migrations/2014_06_12_000708_create_items_table.php
@@ -114,7 +114,7 @@ class CreateItemsTable extends Migration {
 			$table->smallInteger('setassaultslash')->unsigned()->nullable(); //Assault Slash
 			$table->smallInteger('setdemigod')->unsigned()->nullable(); //DEMI GOD
 
-			$table->string('notes', 256)->nullable(); //E.g. part of the nuadha set
+			$table->string('notes', 512)->nullable(); //E.g. part of the nuadha set
 			$table->string('wikilink', 256)->nullable();
 			$table->timestamps();
 		});
