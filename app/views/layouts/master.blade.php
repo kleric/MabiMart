d75e2e50411cb8d976b01601dfdfddafe3f9e53b
Clark Chen <clark@kleric.org>
2014-06-20 16:09:23 -0400

    Only loading lazy load in those that need it

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -54,7 +54,6 @@
 		</div>
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
 		<script src="/js/bootstrap.min.js"></script>
-		<script src="/js/jquery.lazyload.min.js" type="text/javascript"></script>
 		<script>
   			(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
