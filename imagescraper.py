4792038e8c46534c7e04c01c2f36030cae5002c6
MabiMart Dev <dev@mabimart.com>
2014-06-20 13:37:56 -0400

    Added image scraper

---
+++ imagescraper.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+import MySQLdb
+import getpass
+
+hst = raw_input("Host:")
+usr = raw_input("User:")
+passwrd = getpass.getpass()
+database = raw_input("Database:")
+
+db = MySQLdb.connect(host=hst, user=usr, passwd=passwrd,db=database)
+
+cur = db.cursor()
+
+cur.execute("SELECT * FROM items")
+
