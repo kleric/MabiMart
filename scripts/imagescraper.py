c31ad1a5d6e2555ea4cd59f03d41e0dcb07c4dc5
Clark Chen <clark@kleric.org>
2014-06-23 15:26:12 -0400

    Corrected save path

--- scripts/imagescraper.py
+++ scripts/imagescraper.py
@@ -14,12 +14,17 @@ cur = db.cursor()
 
 cur.execute("SELECT id,imgurl FROM items")
 
+numimages = 0
+
 for row in cur.fetchall():
   try:
-    localpath = "public/images/items/" + str(row[0]) + ".png"
+    localpath = "../public/images/items/" + str(row[0]) + ".png"
     urllib.urlretrieve(row[1], localpath)
     print "Downloaded " + row[1] + " successfully!"
+    numimages = numimages + 1
   except IOError, e:
     print str(row[1])
 
 cur.close()
+
+print "Downloaded " + numimages + " images successfully"
