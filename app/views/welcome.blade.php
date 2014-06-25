e828a1aae9a406334f1e0b1848c785cb71fed5a4
Clark Chen <clark@kleric.org>
2014-06-25 13:17:05 -0400

    Brief introduction to prepare for launch

--- app/views/welcome.blade.php
+++ app/views/welcome.blade.php
@@ -4,16 +4,18 @@ Home
 @stop
 @section('content')
 	<br>
-	<div class="alert alert-warning">
-		MabiMart is currently underdevelopment, so don't be surprised if everything explodes suddenly.
-	</div>
-	<div class="page-header">
-		<h1>Welcome!</h1>
-	</div>
-	
 	<div class="jumbotron">
 		<h1>Hello, world!</h1>
 		<p>MabiMart has arrived! An online item trading site for Mabinogi! Finally, a convenient way to buy and sell items with gold.</p>
 		<p><a class="btn btn-success" role="button">Get Trading!</a></p>
+	</div>
+	<div class="page-header">
+		<h1>Welcome!</h1>
+	</div>
+	<div>
+		<p>Hi there, nice to meet you! Feel free to browse the site, list some items, and buy some items! It's honestly painless, I promise!</p>
+		<p>I decided to build the site, with the help of a friend of mine, in order to make a place where people could more easily find items, and sell them. I got sick of depending on housing and party ads in order to find the items I wanted, so I figured, why not a nice place that aggregates all this information?</p>
+		<p>The site has been built over the course of about 3 weeks or so (mostly on the weekends), so it's still kind of a work in progress! If you have any ideas, suggestions, or feedback, checkout the <a href="forum.mabimart.com">forum</a>. I'll be checking it regularly, so fire away!</p>
+		<p>Thanks, Rhaenyx (Alexina)</p>
 	</div>
 @stop
