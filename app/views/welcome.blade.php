9306e9de4efdf9198d27a2d20b49509ec266eb1b
Clark Chen <clark@kleric.org>
2014-06-26 10:42:58 -0400

    Fixed forums url

--- app/views/welcome.blade.php
+++ app/views/welcome.blade.php
@@ -15,7 +15,7 @@ Home
 	<div>
 		<p>Hi there, nice to meet you! Feel free to browse the site, list some items, and buy some items! It's honestly painless, I promise!</p>
 		<p>I decided to build the site, with the help of a friend of mine, in order to make a place where people could more easily find items, and sell them. I got sick of depending on housing and party ads in order to find the items I wanted, so I figured, why not a nice place that aggregates all this information?</p>
-		<p>The site has been built over the course of about 3 weeks or so (mostly on the weekends), so it's still kind of a work in progress! If you have any ideas, suggestions, or feedback, checkout the <a href="forum.mabimart.com">forum</a>. I'll be checking it regularly, so fire away!</p>
+		<p>The site has been built over the course of about 3 weeks or so (mostly on the weekends), so it's still kind of a work in progress! If you have any ideas, suggestions, or feedback, checkout the <a href="http://forums.mabimart.com">forum</a>. I'll be checking it regularly, so fire away!</p>
 		<p>Thanks, Rhaenyx (Alexina)</p>
 	</div>
 @stop
