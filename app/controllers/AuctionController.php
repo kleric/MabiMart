f68adeff78d029722e8585e582ea56dd3272ddaf
Clark Chen <clark@kleric.org>
2014-07-12 03:32:11 -0400

    Changed message

--- app/controllers/AuctionController.php
+++ app/controllers/AuctionController.php
@@ -240,7 +240,7 @@ class AuctionController extends BaseController {
 		else if((isset($autowin) || isset($minprice)) && ($starting_price > $autowin || $starting_price > $min_price))
 		{
 			return Redirect::route('createauction-get', $itemid)
-			->withInput()->with('error_message', 'Your starting price is too high.');
+			->withInput()->with('error_message', 'Your starting price is higher than auto-win.');
 		}
 
 
