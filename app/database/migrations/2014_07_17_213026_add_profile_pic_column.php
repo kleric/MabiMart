91ae81310f035ec4cdbbdff5ad739f559eb2c6df
Clark Chen <clark@kleric.org>
2014-07-18 01:09:05 -0400

    Migration

---
+++ app/database/migrations/2014_07_17_213026_add_profile_pic_column.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddProfilePicColumn extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('users', function($table) {
+			$table->integer('pic_id')->default(0);
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('users', function($table) {
+			$table->dropColumn('pic_id');
+		});
+	}
+
+}
